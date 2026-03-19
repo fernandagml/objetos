ee9c1a0b133749893b55afbcb4311af23541c1f8
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-03-19 11:30:54 -0300

    Criação da estrutura html, css e js.

--- sorteio-numeros-aleatorios/sorteio.js
+++ sorteio-numeros-aleatorios/sorteio.js
@@ -1,33 +1,5 @@
-const form = document.querySelector('.sorteio__form');
-const inputMin = document.querySelector('.sorteio__input--min');
-const inputMax = document.querySelector('.sorteio__input--max');
-const resultado = document.querySelector('.sorteio__resultado--texto');
+const btnSortear = document.querySelector('.area__button');
 
-// Evento do formulário
-form.addEventListener('submit', (event) => {
-    event.preventDefault() // Controla o recarregamento
-
-    const min = inputMin.value.trim();
-    const max = inputMax.value.trim();
-
-    if (min === '' || max === '') {
-        resultado.textContent = 'Preencha os dois campos para realizar o sorteio.';
-        return;
-    }
-
-    const numeroMin = Number(min);
-    const numeroMax = Number(max);
-
-    if (isNaN(numeroMin) || isNaN(numeroMax)) {
-        resultado.textContent = 'Digite apenas números.';
-        return;
-    }
-
-    if (numeroMin > numeroMax) {
-        resultado.textContent = 'O valor mínimo não pode ser maior que o valor máximo.';
-        return;
-    }
-
-    const numeroSorteado = Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin;
-    resultado.textContent = `Número Sorteado: ${numeroSorteado}.`;
+btnSortear.addEventListener('click', () => {
+    alert('Olá Ivo')
 })
