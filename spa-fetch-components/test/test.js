9320fb8f400ec431ec0ce01693932b161283fb89
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-04-24 08:59:51 -0300

    Teste 1 JS finalizado.

---
+++ spa-fetch-components/test/test.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 'banana';
+
+try {
+    x = y + 1;
+} catch(erro) {
+    let text = `
+    <strong>Nome do erro:</strong> ${erro.name} <br />
+    <strong>Mensagem:</strong> ${erro.message} <br />
+    <strong>Stack: </strong><pre>${erro.stack}</pre>
+    `;
+
+    document.body.innerHTML = text
+};
